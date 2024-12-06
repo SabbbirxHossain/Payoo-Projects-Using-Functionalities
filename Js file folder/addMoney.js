389c30ad035df20bdccf1637e95b56ba27a0cdd7
unknown <sanmoon@gmail.com>
2024-12-06 12:22:45 +0600

    get the value of amount and pin by function are added

--- Js file folder/addMoney.js
+++ Js file folder/addMoney.js
@@ -1,5 +1,9 @@
 document.getElementById('btn-add-money').addEventListener('click',function(event){
   event.preventDefault();
 
-  
+  const add_Money = getInputFieldValueById('input-add-money');
+  const add_Pin = getInputFieldValueById('input-pin-number');
+  console.log(add_Money ,add_Pin);
+
+ 
 })
---
+++ Js file folder/utilities.js
@@ -0,0 +1,14 @@
+/**
+ * Common Shared function
+*/
+
+
+// function getInputFieldValueById(){
+//   const addMoney = document.getElementById('input-add-money').value;
+//   return addMoney;
+// }
+
+function getInputFieldValueById(id){
+  const inputValue = document.getElementById(id).value;
+  return inputValue;
+}

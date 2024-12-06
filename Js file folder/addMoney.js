f42716576cbc85a1283bcef6a413321038bea0cd
unknown <sanmoon@gmail.com>
2024-12-06 12:33:13 +0600

    get the value of amount and pin by function are added again

--- Js file folder/addMoney.js
+++ Js file folder/addMoney.js
@@ -2,8 +2,14 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
   event.preventDefault();
 
   const add_Money = getInputFieldValueById('input-add-money');
-  const add_Pin = getInputFieldValueById('input-pin-number');
-  console.log(add_Money ,add_Pin);
+  const pinNumber = getInputFieldValueById('input-pin-number');
+  // console.log(add_Money ,add_Pin);
+
+  if(pinNumber === 1234){
+
+  }else{
+    alert("Please try again.Invalid pin or Amount");
+  }
 
  
 })
--- Js file folder/utilities.js
+++ Js file folder/utilities.js
@@ -10,5 +10,6 @@
 
 function getInputFieldValueById(id){
   const inputValue = document.getElementById(id).value;
-  return inputValue;
+  const convertIntoFloat = parseFloat(inputValue);
+  return convertIntoFloat;
 }
